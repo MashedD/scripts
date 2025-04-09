13639c870f07ac8b64531d4c1d50fa9aebca6b1e
MashedD <sl8code@gmail.com>
2025-04-09 21:57:18 +0200

    Update

--- update-all.sh
+++ update-all.sh
@@ -6,12 +6,38 @@ set -Eeuo pipefail
 ./update-crispyhexen.sh
 ./update-dethrace.sh
 ./update-ironwail.sh
-./update-joequake.sh
+#./update-joequake.sh
 ./update-lastermaul.sh
-./update-librequake-ironwail.sh
+#./update-librequake-ironwail.sh
 #./update-librequake.sh
 ./update-notblood.sh
 ./update-q2pro.sh
 ./update-q2repro.sh
+#./update-vkquake.sh
+
+# Consider:
+#
+# 3zb2-zigflag
+# Anarch
+# deutex
+# dmenu
+# dzip
+# eduke32
+# gzdoom
+# openmohaa
+# qpakman
+# qrustyquake
+# QSprawl
+# QSS-M
+# Quake3e
+# QuakeSpasm-Spiked
+# QuakeSpasm
+# r96
+# Raze
+# SDL
+# thgtoa
+# TrenchBroom
+# zmusic
+# ...
 
 
