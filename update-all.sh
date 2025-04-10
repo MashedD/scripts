c148d3e492a4f01f0cf496e97902f55493adf8cb
MashedD <sl8code@gmail.com>
2025-04-10 17:35:04 +0200

    Change permissions

--- update-all.sh
+++ update-all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-./update-blasphemer.sh
-./update-crispydoom.sh
-./update-crispyheretic.sh
-./update-crispyhexen.sh
-./update-dethrace.sh
+#./update-blasphemer.sh
+#./update-crispydoom.sh
+#./update-crispyheretic.sh
+#./update-crispyhexen.sh
+#./update-dethrace.sh
 ./update-ironwail.sh
 #./update-joequake.sh
-./update-lastermaul.sh
+#./update-lastermaul.sh
 #./update-librequake-ironwail.sh
 #./update-librequake.sh
-./update-notblood.sh
+#./update-notblood.sh
 ./update-q2pro.sh
 ./update-q2repro.sh
 #./update-vkquake.sh
