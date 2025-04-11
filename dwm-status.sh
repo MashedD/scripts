666e23d2252ad5f042e335b08de309a3e78c1cc5
MashedD <sl8code@gmail.com>
2025-04-11 20:46:39 +0200

    Rem zzz, set script dir in update-all, update dwm-status

--- dwm-status.sh
+++ dwm-status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-
+export LC_ALL=pl_PL.UTF-8
 while true; do
     xsetroot -name "$(date)"
     sleep 1
--- update-all.sh
+++ update-all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+cd $(dirname "$0")
 #./update-blasphemer.sh
 #./update-crispydoom.sh
 #./update-crispyheretic.sh
