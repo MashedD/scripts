d1e905b5745716809d1e3d40341989676c2b8ebc
MashedD <sl8code@gmail.com>
2025-04-15 21:53:47 +0200

    q2pro: change mouse DPI

--- play-q2pro.sh
+++ play-q2pro.sh
@@ -6,7 +6,7 @@ ORIGINAL_RESOLUTION=$(xrandr --current | grep '*' | awk '{print $1}')
 CONNECTED_DISPLAY=$(xrandr | grep ' connected' | awk '{print $1}' | head -n 1)
 xrandr --output "$CONNECTED_DISPLAY" --mode 640x480
 ORIGINAL_DPI=$(ratbagctl G102 dpi get | sed 's/dpi//g')
-ratbagctl G102 dpi set 3000
+ratbagctl G102 dpi set 1000
 ./q2pro +set homedir ./ \
     +exec mashedd-common.cfg \
     +exec mashedd-player.cfg \
