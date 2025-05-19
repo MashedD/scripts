f90e8b490d5a751f11f6af8c63c1726cdb04abf2
MashedD <sl8code@gmail.com>
2025-05-19 09:15:25 +0200

    change play scripts

--- play-q2pro.sh
+++ play-q2pro.sh
@@ -15,7 +15,7 @@ TOUCHPAD_NAME="ELAN0300:00 04F3:3206 Touchpad"
 xinput disable "$TOUCHPAD_NAME"
 
 # Start game
-./q2pro +set homedir . \
+gamescope -W 1920 -H 1080 -r 144 --force-grab-cursor -f -- ./q2pro +set homedir . \
     +exec mashedd-common.cfg \
     $*
 
--- play-syberia.sh
+++ play-syberia.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
-
-# Display resolution
-ORIGINAL_RESOLUTION=$(xrandr --current | grep '*' | awk '{print $1}')
-CONNECTED_DISPLAY=$(xrandr | grep ' connected' | awk '{print $1}' | head -n 1)
-xrandr --output "$CONNECTED_DISPLAY" --mode 800x600
-
 env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/32
-
-# Restore display resolution
-xrandr --output "$CONNECTED_DISPLAY" --mode "$ORIGINAL_RESOLUTION"
 
